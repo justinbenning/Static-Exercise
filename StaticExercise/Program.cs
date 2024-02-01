0f56cb8932b7b7072a2b9b1947dd28a49c03c23a
JustinBenning <justin.benning@gmail.com>
2024-02-01 18:09:43 -0500

    project upload

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(0));
+            Console.WriteLine(TempConverter.FahrenheitToCelsius());
+            
+
+
         }
+
     }
+
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public class TempConverter
+    {
+        public static double FahrenheitToCelsius()
+        {
+            Console.WriteLine("Please enter a fahrenheit temperature.");
+            double f = double.Parse(Console.ReadLine()!);
+            double celsius = 5 / 9 * (f - 32);
+            return celsius;
+            
+        }
+        public static double CelsiusToFahrenheit(double c)
+        {
+            double fahrenheit = (c * 9 / 5) + 32;
+            return fahrenheit;
+        }
+    }
+}
